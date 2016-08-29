87f6e4c9ba3f9fed91f336ee387411aafdf091c7
panpanlt <328032704@qq.com>
2016-08-29 11:14:13 +0800

    0.0.8

--- src/com/bwei/test/Test.java
+++ src/com/bwei/test/Test.java
@@ -2,7 +2,7 @@ package com.bwei.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("第一个测试git方法");
+		System.out.println("branch first test");
 		System.out.println("my test first branch");
 		System.out.println("this is my git project");
 		System.out.println("that's my second test");
