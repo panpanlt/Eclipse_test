bc36706c859518778548929afdc42955770b4782
panpanlt <328032704@qq.com>
2016-08-29 10:06:01 +0800

    0.0.3

--- src/com/bwei/test/Test.java
+++ src/com/bwei/test/Test.java
@@ -3,5 +3,7 @@ package com.bwei.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("第一个测试git方法");
+		System.out.println("my test first branch");
+		System.out.println("this is my git project");
 	}
 }
